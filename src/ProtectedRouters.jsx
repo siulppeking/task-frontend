4686c9acf81110d75662edead27f2afb98f86052
Luis Escobar <dukz@MacBook-Pro-de-Luis.local>
2023-07-21 21:22:50 -0500

    add loading in Protected Routers

--- src/ProtectedRouters.jsx
+++ src/ProtectedRouters.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useAuth } from './context/AuthContext'
 import { Navigate, Outlet } from 'react-router-dom';
+import { Loading } from './components/Loading';
 
 export const ProtectedRouters = () => {
     const { loading, isAuthenticaded } = useAuth();
 
-    if (loading) return <></>;
+    if (loading) return <Loading />;
     if (!isAuthenticaded && !loading) return <Navigate to="/login" replace />;
 
     return (<Outlet />);
